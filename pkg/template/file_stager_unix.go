665ad52ab4279eb81b834dee2540fd02a3d565db
abtreece <britt.treece@gmail.com>
2026-04-18 22:56:31 -0500

    feat: add contentUnchanged() to fileStager for in-memory content comparison

--- pkg/template/file_stager_unix.go
+++ pkg/template/file_stager_unix.go
@@ -2,9 +2,22 @@
 
 package template
 
-import "os"
+import (
+	"os"
+	"syscall"
+)
 
 // chownFile sets the owner and group of the named file.
 func chownFile(path string, uid, gid int) error {
 	return os.Chown(path, uid, gid)
+}
+
+// destOwnershipMatches reports whether the dest file's uid and gid match
+// the expected values. Returns false if the syscall info is unavailable.
+func destOwnershipMatches(stat os.FileInfo, uid, gid int) bool {
+	sys, ok := stat.Sys().(*syscall.Stat_t)
+	if !ok {
+		return false
+	}
+	return int(sys.Uid) == uid && int(sys.Gid) == gid
 }
