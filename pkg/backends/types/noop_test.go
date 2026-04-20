f324fa0826b8a4925245e699871a811385cec6dd
abtreece <britt.treece@gmail.com>
2026-04-19 21:23:15 -0500

    refactor: add NoopWatcher and NoopCloser embeddable structs to types package

---
+++ pkg/backends/types/noop_test.go
@@ -0,0 +1,44 @@
+package types_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/abtreece/confd/pkg/backends/types"
+)
+
+func TestNoopWatcher_StopChan(t *testing.T) {
+	var w types.NoopWatcher
+	stopChan := make(chan bool, 1)
+	stopChan <- true
+
+	idx, err := w.WatchPrefix(context.Background(), "/prefix", nil, 42, stopChan)
+	if err != nil {
+		t.Fatalf("expected nil error, got %v", err)
+	}
+	if idx != 42 {
+		t.Fatalf("expected waitIndex 42, got %d", idx)
+	}
+}
+
+func TestNoopWatcher_ContextCanceled(t *testing.T) {
+	var w types.NoopWatcher
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	stopChan := make(chan bool)
+	idx, err := w.WatchPrefix(ctx, "/prefix", nil, 42, stopChan)
+	if err != context.Canceled {
+		t.Fatalf("expected context.Canceled, got %v", err)
+	}
+	if idx != 42 {
+		t.Fatalf("expected waitIndex 42, got %d", idx)
+	}
+}
+
+func TestNoopCloser_Close(t *testing.T) {
+	var c types.NoopCloser
+	if err := c.Close(); err != nil {
+		t.Fatalf("expected nil error, got %v", err)
+	}
+}
